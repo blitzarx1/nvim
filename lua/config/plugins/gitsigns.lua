bc9447885ac3cdd7d714729aea2bc4d887b803d6
Dmitrii Samsonov <dmitrii.samsonov@exness.com>
2026-01-09 14:46:03 +0200

    Fix gitsigns

--- lua/config/plugins/gitsigns.lua
+++ lua/config/plugins/gitsigns.lua
@@ -1,11 +1,10 @@
 return {
   {
     'lewis6991/gitsigns.nvim',
-    opts = {
-      current_line_blame = true,
-    },
     config = function()
-      local gs = require('gitsigns')
+      local gs = require('gitsigns').setup({
+        current_line_blame = true, 
+      })
       local opts  = { noremap = true, silent = true }
       vim.keymap.set('n', ']g', function() gs.nav_hunk('next') end, opts)
       vim.keymap.set('n', '[g', function() gs.nav_hunk('prev') end, opts)
