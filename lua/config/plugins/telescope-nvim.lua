ccb904d707849903466c8a1635f499a639a4b1eb
starlod <blitzar90@gmail.com>
2026-01-13 17:44:08 +0200

    Keymaps

--- lua/config/plugins/telescope-nvim.lua
+++ lua/config/plugins/telescope-nvim.lua
@@ -7,8 +7,8 @@ return{{
     },
     config = function()
       local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files' })
+      vim.keymap.set('n', '<leader>/', builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'Telescope buffers' })
     end,
 }}
