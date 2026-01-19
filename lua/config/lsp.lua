ec2891953a50f88c1e223099120c6763d0a8010e
starlord <blitzar90@gmail.com>
2026-01-19 08:23:25 +0200

    Fix C-k

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -14,7 +14,7 @@ map('n', '<leader>k', vim.lsp.buf.hover, opts)
 map('n', '<leader>a', vim.lsp.buf.code_action, opts)
 map('n', '<leader>d', vim.diagnostic.open_float, opts)
 map('n', '<leader>D', function() vim.diagnostic.setqflist({ open = true }) end, opts)
-map({'n', 'i'}, '<C-k>', vim.lsp.buf.signature_help, opts)
+map('i', '<C-k>', vim.lsp.buf.signature_help, opts)
 
 -- disable diagnostic
 vim.diagnostic.enable(false)
