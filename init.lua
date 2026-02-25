d119a49881d1581410e4afcdc1bde091ef3df5b4
blitzar90 <blitzar90@gmail.com>
2026-02-25 16:37:59 +0200

    treesitter context

--- init.lua
+++ init.lua
@@ -169,7 +169,30 @@ require"oil".setup({
 vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" } )
 
 vim.pack.add({ {src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "master"} })
+local parser_install_dir = vim.fn.stdpath("data") .. "/site"
+vim.opt.runtimepath:prepend(parser_install_dir)
 require"nvim-treesitter.configs".setup({
+  parser_install_dir = parser_install_dir,
+  ensure_installed = {
+    "lua",
+    "vim",
+    "vimdoc",
+    "query",
+    "bash",
+    "python",
+    "javascript",
+    "typescript",
+    "tsx",
+    "json",
+    "yaml",
+    "toml",
+    "go",
+    "c",
+    "cpp",
+    "markdown",
+    "markdown_inline",
+  },
+  auto_install = true,
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
@@ -185,4 +208,21 @@ require"nvim-treesitter.configs".setup({
       node_decremental = "<S-m>",
     },
   },
+})
+
+vim.pack.add({{src = "https://github.com/nvim-treesitter/nvim-treesitter-context"}})
+require"treesitter-context".setup({
+    enable = true,
+    multiwindow = false,      -- Enable multiwindow support.
+    max_lines = 5,            -- How many lines the window should span. Values <= 0 mean no limit.
+    min_window_height = 0,    -- Show in any window height.
+    line_numbers = true,
+    multiline_threshold = 20, -- Maximum number of lines to show for a single context
+    trim_scope = "outer",     -- Which context lines to discard if `max_lines` is exceeded. Choices: "inner", "outer"
+    mode = "cursor",          -- Line used to calculate context. Choices: "cursor", "topline"
+    zindex = 20,              -- The Z-index of the context window
+    on_attach = nil,          -- (fun(buf: integer): boolean) return false to disable attaching
+    -- Separator between context and content. Should be a single character string, like '-'.
+    -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+    separator = '-',
 })
