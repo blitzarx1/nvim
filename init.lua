c82a9c41d81ee229272b092edf397b45a7f96e92
blitzar90 <blitzar90@gmail.com>
2026-04-17 20:30:33 +0300

    chore: doc about showind special chars

--- init.lua
+++ init.lua
@@ -21,7 +21,8 @@ vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
 vim.o.expandtab = true
 
-vim.o.list = true
+-- to show / hide special characters use
+-- :set list / :set nolist correspondingly
 vim.o.listchars = "tab:▸ ,space:·,trail:·,extends:›,precedes:‹,eol:$"
 
 vim.o.ignorecase = true
